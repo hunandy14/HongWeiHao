778b1feda78251eab51194ec3f7ebf71fd23fbcc
CharlotteHonG <hunandy14@gmail.com>
2016-05-15 00:56:41 +0800

    動態創建 一維 或 二維 String

--- string_creator.cpp
+++ string_creator.cpp
@@ -24,18 +24,24 @@ int main(int argc, char const *argv[]){
 	/* 創建方法 - 注意星號數量 */
 	string *str1=string_creator(16);
 	string **str2=string_creator(4, 2);
+	
 	/* 使用範例1 */
 	str1[15]="[str1]Charloote";
 	cout << "str1[15]      = ";
 	cout << str1[15] << endl;
 	cout << "str1[15][4]   = ";
 	cout << str1[15][4] << endl;
+	// 歸還
+	delete [] str1;
+
 	/* 使用範例2 */
 	str2[3][1]="[str2]Charloote";
 	cout << "str2[3][1]    = ";
 	cout << str2[3][1] << endl;
 	cout << "str2[3][1][4] = ";
 	cout << str2[3][1][4] << endl;
+	// 歸還
+	delete [] str2;
 	return 0;
 }
 /*=======================================================*/
