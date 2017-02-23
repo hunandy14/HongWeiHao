6bf23cb0d537a2a259d43b9f054d15856f97d4d4
CharlotteHonG <hunandy14@gmail.com>
2017-02-23 17:40:15 +0800

    重載 operator+ 與 += 函式
    說明：https://charlottehong.blogspot.tw/2017/02/c-operator.html

---
+++ operator_add.cpp
@@ -0,0 +1,59 @@
+/*****************************************************************
+Name : 重載 operator+ 與 += 函式
+Date : 2017/02/22
+By   : CharlotteHonG
+Final: 2017/02/22
+*****************************************************************/
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Arr{
+public:
+    Arr(size_t len=3, int value=1): num(len) {
+        cout << "create" << endl;
+        for(auto&& i : num)
+            i=value;
+    }
+    Arr & operator+=(Arr const &rhs){
+        for(unsigned i = 0; i < num.size(); ++i)
+            this->num[i] += rhs.num[i];
+        return *this;
+    }
+    Arr & operator+=(int value){
+        for(unsigned i = 0; i < num.size(); ++i)
+            this->num[i] += value;
+        return *this;
+    }
+    void pri(){
+        for(auto&& i : num) {
+            cout << i << ", ";
+        }cout << endl;
+    }
+    vector<int> num;
+};
+
+Arr operator+(Arr const &lhs, Arr const &rhs){
+    cout << "lhs+rhs : ";
+    return Arr(lhs) += rhs;
+}
+Arr operator+(int value, Arr const &rhs){
+    cout << "rhs+    : ";
+    return Arr(rhs) += value;
+}
+Arr operator+(Arr const &lhs, int value){
+    cout << "lhs+    : ";
+    return Arr(lhs) += value;
+}
+/*==============================================================*/
+int main(int argc, char const *argv[]){
+    Arr p;
+    p=p+p;
+    p.pri();
+    p=1+p;
+    p.pri();
+    p=p+1;
+    p.pri();
+    return 0;
+}
+/*==============================================================*/
