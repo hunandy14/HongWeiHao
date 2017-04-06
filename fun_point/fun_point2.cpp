5ae26546694fb6e400be76b9a2cf538f6c57985d
CharlotteHonG <hunandy14@gmail.com>
2017-04-06 17:05:38 +0800

    函式指標

---
+++ fun_point/fun_point2.cpp
@@ -0,0 +1,45 @@
+/*****************************************************************
+Name : 
+Date : 2017/04/04
+By   : CharlotteHonG
+Final: 2017/04/04
+*****************************************************************/
+#include <iostream>
+using namespace std;
+
+int add(int a, int b){
+    return a+b;
+}
+int sub(int a, int b){
+    return a-b;
+}
+
+// 為宣告建立別名
+// typedef int (*p)(int, int);
+// using p = int (*)(int, int);
+
+// 搭配 decltype
+// typedef decltype(add) p;
+using p = decltype(add);
+
+
+// 使用 p 宣告
+int fun(int a, int b, auto fun_point){
+    return (*fun_point)(a, b);
+}
+// 直接使用
+int fun2(int a, int b, decltype(add) fun){
+    return (*fun)(a, b);
+}
+//================================================================
+int main(int argc, char const *argv[]){
+    cout << fun(2, 3, &add) << endl;
+    cout << fun(2, 3, &sub) << endl;
+
+    // 主程式取別名
+    auto p=add;
+    cout << fun2(2, 3, p) << endl;
+
+    return 0;
+}
+//================================================================
