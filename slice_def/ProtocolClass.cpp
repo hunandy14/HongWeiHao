c90bbc5f56375786954483e64343a25aee18df1b
Charlotte.HonG <hunandy14@gmail.com>
2017-05-15 01:10:06 +0800

    修正屬性關係

--- slice_def/ProtocolClass.cpp
+++ slice_def/ProtocolClass.cpp
@@ -2,7 +2,7 @@
 Name : 
 Date : 2017/05/04
 By   : CharlotteHonG
-Final: 2017/05/04
+Final: 2017/05/05
 *****************************************************************/
 #include <iostream>
 using namespace std;
@@ -11,38 +11,45 @@ class Name;
 class Tw{
 public:
     virtual ~Tw(){}
-    virtual Name name()=0;
-    static Tw* makeTw(string const & name);
+    virtual string name()=0;
+    static Tw* makeTw(Name const & name);
 };
+
 // Name class定義
 class Name{
 public:
     Name(string const & n);
+    operator string&(){
+        return n;
+    }
     friend ostream& operator<<(ostream& out, Name const & n);
 private:
     string n;
 };
-
+//================================================================
+int main(int argc, char const *argv[]){
+    Tw* p;
+    Name chg = string("chg");
+    p = Tw::makeTw(chg);
+    cout << p->name() << endl;
+    return 0;
+}
+//================================================================
 
 // Tw class定義
 class RealTw: public Tw{
 public:
-    RealTw(string const & name): n(name){}
-    Name name(){return n;}
+    RealTw(Name const & name): n(name){}
+    string name(){return n;}
 private:
     Name n;
 };
-//================================================================
-int main(int argc, char const *argv[]){
-    Tw* p = Tw::makeTw("chg");
-    // cout << p->name() << endl;
-    return 0;
-}
-//================================================================
+
 // makeTw 函式定義
-Tw* Tw::makeTw(string const & name){
+Tw* Tw::makeTw(Name const & name){
     return new RealTw(name);
 }
+
 // Name 函式定義
 Name::Name(string const & n): n(n){}
 ostream& operator<<(ostream& out, Name const & n){
